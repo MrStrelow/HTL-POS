5d26117225594f542d86b7ce0435efcf8391ba27
MrStrelow <mathias.cammerlander@gmail.com>
2026-09-21 09:04:10 +0200

    Live Coding.

--- JahrGang2/L01LiveCodingWiederholung/L01Schachbrett/Program.cs
+++ JahrGang2/L01LiveCodingWiederholung/L01Schachbrett/Program.cs
@@ -9,9 +9,24 @@ for (int y = 0; y < 6; y++)
     for (int x = 0; x < 6; x++)
     {
         //bool isWhiteSymbol = (y % 2 == 0 && x % 2 == 0) || (y % 2 == 1 && x % 2 == 1);
-        bool isRedSymbol    = (x + y) % 3 == 0;
-        bool isBlackSymbol  = (x + y) % 3 == 1;
-        bool isWhiteSymbol  = (x + y) % 3 == 2;
+        //bool isRedSymbol    = (x + y) % 3 == 0;
+        //bool isBlackSymbol  = (x + y) % 3 == 1;
+        //bool isWhiteSymbol  = (x + y) % 3 == 2;
+
+        bool isWhiteSymbol = 
+            (y % 3 == 0 && x % 3 == 2) || 
+            (y % 3 == 1 && x % 3 == 1) || 
+            (y % 3 == 2 && x % 3 == 0);
+
+        bool isRedSymbol = 
+            () || 
+            () || 
+            ();
+
+        bool isBlackSymbol = 
+            () || 
+            () || 
+            ();
 
         if (isWhiteSymbol) 
         {
