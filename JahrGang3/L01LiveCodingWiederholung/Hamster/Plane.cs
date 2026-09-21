a41663a9dfd5df679e2b7ee309615b06e6642ae2
MrStrelow <mathias.cammerlander@gmail.com>
2026-09-21 12:19:53 +0200

    Live Coding.

--- JahrGang3/L01LiveCodingWiederholung/Hamster/Plane.cs
+++ JahrGang3/L01LiveCodingWiederholung/Hamster/Plane.cs
@@ -3,9 +3,34 @@
 public class Plane
 {
     // Felder
+    private string[,] _plane;
+    private string _earthRepresentation = "🟫";
+
     // Eigenschaften
+    public int Size { get; private set; }
     // Konstruktoren
+    public Plane(int sizeOfPlane)
+    {
+        Size = sizeOfPlane;
+        _plane = new string[Size, Size];
+
+        // Befülle die _plane mit einem Erdesymbol 🟫
+        for (int zeile = 0; zeile < Size; zeile++)
+        {
+            for (int spalte = 0; spalte < Size; spalte++)
+            {
+                _plane[zeile, spalte] = _earthRepresentation;
+            }
+        }
+
+        // Was nun?
+    }
+
     // Methoden
+    public void Print()
+    {
+
+    }
+
     // private Methoden
-    string what;
 }
--- JahrGang3/L01LiveCodingWiederholung/Hamster/Program.cs
+++ JahrGang3/L01LiveCodingWiederholung/Hamster/Program.cs
@@ -1,3 +1,25 @@
-﻿using Hamster;
+﻿// userinput -
+// * eingabe,
+// * prüfen ob sinnvoll und solange nachfragen bis sinnvoll
 
-Console.WriteLine("what");
+using System.Numerics;
+
+int sizeOfPlane;
+string promptForUser = "Wie groß soll die Wiese sein?: ";
+Console.Write(promptForUser);
+
+while (!int.TryParse(Console.ReadLine(), out sizeOfPlane) || sizeOfPlane <= 0)
+//while (!(int.TryParse(Console.ReadLine(), out sizeOfPlane) && sizeOfPlane > 0))
+{
+    Console.Clear();
+
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Input is not an integer number. Please try again.");
+    Console.ResetColor();
+
+    Console.Write(promptForUser);
+}
+
+// Plane objekt erstellen und aufgabe das spiel zu starten
+// * eingabe der größe vom user an Konstruktor übergeben.
+Plane plane = new Plane(sizeOfPlane);
