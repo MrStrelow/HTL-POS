2f5b5fad3c9dd0e000cff2b21df13a9823aadf5c
MrStrelow <mathias.cammerlander@gmail.com>
2026-09-23 09:57:26 +0200

    Live Coding

--- JahrGang3/L01LiveCodingWiederholung/Hamster/Plane.cs
+++ JahrGang3/L01LiveCodingWiederholung/Hamster/Plane.cs
@@ -8,6 +8,11 @@ public class Plane
 
     // Eigenschaften
     public int Size { get; private set; }
+
+    // Beziehungen
+    // 1 zu N hat-Beziehung: umgesetzt mit einem Dictionary (könnte auch Liste oder Array oder... sein)
+    private Dictionary<(int x, int y), Seedling> _seedlings = new ();
+
     // Konstruktoren
     public Plane(int sizeOfPlane)
     {
@@ -29,12 +34,30 @@ public class Plane
         for (int i = 0; i < numberOfSeedings; i++)
         {
             Seedling seedling = new Seedling();
+            _seedlings[seedling.Position] = seedling;
         }
     }
 
     // Methoden
     public void Print()
     {
+
+
+
+
+
+        while (false)
+        {
+
+        }
+
+
+
+
+
+
+
+
         for (int zeile = 0; zeile < Size; zeile++)
         {
             for (int spalte = 0; spalte < Size; spalte++)
--- JahrGang3/L01LiveCodingWiederholung/Hamster/Seedling.cs
+++ JahrGang3/L01LiveCodingWiederholung/Hamster/Seedling.cs
@@ -4,6 +4,9 @@ public class Seedling
 {
     // Felder
     // Eigenschaften
+    public Plane Plane { get; private set; }
+    public (int x, int y) Position { get; private set; }
+    // Beziehungen
     // Konstruktoren
     // Methoden
     // private Methoden
